1a91ccb890e2939d4d5cace96f19b96af874b351
Martin Thodi <martthodi238@gmail.com>
2024-04-11 23:02:59 +0200

    routeviews processing pipeline

--- parse_rv_data.py
+++
@@ -1,6 +0,0 @@
-# ============================================================
-# Description:
-# This script parses the RV data from the downloaded MRT files.
-# ============================================================
-
-import pandas as pd
--- parse_rv_files.py
+++ parse_rv_files.py
@@ -1,16 +1,16 @@
 import pandas as pd
 import config
 import bgpkit
-import manage_storage
 from tqdm import tqdm
+from concurrent.futures import ProcessPoolExecutor, as_completed
 import os
 
-base_dir = config.base_dir
-rv_raw_dir = base_dir / 'routeviews' / 'rv_raw/napafrica'
+base_dir = config.BASE_DATA_DIR
+rv_raw_dir = base_dir / 'routeviews/rv_raw/'
+output_dir = base_dir / 'paths/latest/routeviews/'
 assert base_dir.exists()
 assert rv_raw_dir.exists()
-output_engine = config.make_engine('routes')
-fm = manage_storage.FileManager(base_dir / 'routeviews')
+assert output_dir.exists()
 
 
 # functions
@@ -37,36 +37,44 @@ def parse_mrt_file(file_path):
     output_file_name = file_path.split('/')[-1]
     parser = bgpkit.Parser(url=file_path)
 
-    elements = parser.parse_all()
-    rc_name = output_file_name.split('_')[0]
+    elements = []
+    more_items = True
+    while more_items:
+        e = parser.parse_next()
+        if e is None:
+            more_items = False
+        else:
+            # check if prefix is ipv6
+            if ':' in e['prefix']:
+                break
+            elements.append(e)
     _df = pd.DataFrame(elements)
     if _df.empty:
         return 0
     _df['origin_asns'] = _df['origin_asns'].apply(flatten_list_to_string)
     _df['communities'] = _df['communities'].apply(flatten_list_to_string)
-    _df['route_collector_name'] = rc_name
-    _df['timestamp'] = pd.to_datetime(_df['timestamp'], unit='s')
     # pick only columns we need
-    _df_db = _df[['timestamp', 'peer_ip', 'peer_asn', 'prefix', 'as_path', 'origin_asns', 'origin', 'communities',
-                  'route_collector_name', ]]
+    _df_db = _df[['peer_ip', 'peer_asn', 'prefix', 'as_path', 'origin_asns', 'origin', 'communities',
+                  'atomic', 'aggr_asn', 'aggr_ip']]
     # save to db
-    tt = output_file_name.split('.bz2')[0]
-    _date = tt.split('_')[3] + "-" + tt.split('_')[1] + "-" + tt.split('_')[2]
-    output_dir = fm.get_directory_name(_date)
+    output_file_name = output_file_name.split('.bz2')[0]
     _df.to_parquet(f"{output_dir}/{output_file_name}.parquet")
-    # delete file
-    os.remove(file_path)
 
 
 def main():
     rv_raw_files = list(rv_raw_dir.glob('*'))
     print(f"Number of files: {len(rv_raw_files)}")
-    for mrt_file in tqdm(rv_raw_files[10:20]):
-        try:
-            print(f">>> Parsing {mrt_file}")
-            parse_mrt_file(mrt_file)
-        except Exception as e:
-            print(f"An error occurred: {str(e)}")
+
+    batch_size = 2
+    for i in range(0, len(rv_raw_files), batch_size):
+        batch = rv_raw_files[i:i + batch_size]
+        with ProcessPoolExecutor() as executor:
+            futures = {executor.submit(parse_mrt_file, mrt_file) for mrt_file in tqdm(batch)}
+            for future in tqdm(as_completed(futures), total=len(futures)):
+                try:
+                    r = future.result()
+                except Exception as e:
+                    print(f"An error occurred: {str(e)}")
 
 
 if __name__ == '__main__':
